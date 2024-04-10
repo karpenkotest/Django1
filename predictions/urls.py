5d9a53b4f9452794fed472dad64fbd79f47ea7bf
karpenkotest <karpenkotest@gmail.com>
2024-04-10 14:14:39 +0300

    second hw

--- predictions/urls.py
+++ predictions/urls.py
@@ -1,15 +1,26 @@
 from django.urls import path
+
 from .views import predictions_view
 from .views import empty_view
 from .views import random_view
 from .views import index_view
 from .views import nested_view
 from .views import depends_on_number_in_url_view
+from .views import dashboard_view
+from .views import writers_view
+from .views import article_view
+
+
+
 urlpatterns = [
     path('prediction/',predictions_view),
-     path('', empty_view),
+    path('', empty_view, name="MAIN_PAGE"),
     path('random/', random_view),
-    path('index/', index_view),
+    path('index/', index_view, name='index'),
+    path('dashbord/', dashboard_view, name='dashboard'),
+    path('writers/', writers_view, name='Writers'),
     path('nested/new', nested_view),
-    path('<int:some_number>', depends_on_number_in_url_view)
+    path('article/', article_view, name='article'),
+path('article/<int:some_number>/', depends_on_number_in_url_view, name="random_article"),
+    path('article/<int:some_number>/<slug:slug_text>/', depends_on_number_in_url_view, name="random_article")
 ]
--- predictions/views.py
+++ predictions/views.py
@@ -1,23 +1,84 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
+from random import randint
+
+
+def depends_on_number_in_url_view(request,some_number, slug_text=None):
+    result=""
+    if (some_number%2)==0:
+        result="Even"
+    else:
+        result="Odd"
+
+    context={'result':result,
+             'text':slug_text,
+             'number':randint(0,100)
+    }
+    return render(request,'depends_on_number.html', context)
+
+
 def predictions_view(request):
     return HttpResponse('You will have a great day! Have fun!')
 def empty_view(request):
     return HttpResponse('If you want to see your future - add /prediciton or /random to the url')
 
 def random_view(request):
-    import random
-    random_number = random.randint(0, 5)
-    if random_number == 0: return HttpResponse("Stay at home")
-    if random_number == 2: return HttpResponse("Go to work")
-    if random_number ==3: return HttpResponse("Go to gym")
-    if random_number == 4: return  HttpResponse( "Buy a ticket to Ukraine")
-    if random_number == 5: return HttpResponse("Buy a ticket to Paris")
-
+    random_number = randint(0, 6)
+    response = 'Learn Python'
+    response_dict={
+        0:"Stay at home",
+        1:"Go to work",
+        2:"Go to gym",
+        3: "Buy a ticket to Ukraine",
+        4: "Buy a ticket to Paris",
+        5: "Drink a coffe",
+        6: "Watch a favorite film"
+    }
+    response=response_dict.get(random_number)
+    return HttpResponse(response)
 def nested_view(request):
     return HttpResponse("Simple test")
 
+def index_view1(request):
+    context = {"time": datetime.now()}
+    return render(request, 'index.html', context)
+
+def dashboard_view(request):
+    return render(request, 'dashboard.html')
+
+def writers_view(request):
+    return render(request, 'writers.html')
+
+def article_view(request):
+    return render(request, 'Article.html')
+
+
+class MyClass:
+    string = ''
+
+    def __init__(self, s):
+        self.string = s
+
 def index_view(request):
-    return render(request, 'index.html')
-def depends_on_number_in_url_view(request,some_number):
-    return HttpResponse(f'Number in ulr is {some_number}')
+    my_num = 33
+    my_str = 'Yuliia'
+    my_dict = {"some_key": "some_value"}
+    my_list = ['list_first_item', 'list_second_item', 'list_third_item']
+    my_set = {'set_first_item', 'set_second_item', 'set_third_item'}
+    my_tuple = ('tuple_first_item', 'tuple_second_item', 'tuple_third_item')
+    my_class = MyClass('class string')
+    data = {
+        'my_num': my_num,
+        'my_str': my_str,
+        'my_dict': my_dict,
+        'my_list': my_list,
+        'my_set': my_set,
+        'my_tuple': my_tuple,
+        'my_class': my_class,
+    }
+    return render(
+        request,
+        'index.html',
+        data
+    )
